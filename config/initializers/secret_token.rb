cca19ce0e875c8e854bcb43e9852918fbe16f50f
michel <cali77@hotmail.fr>
2013-07-01 23:38:31 +0200

    depot secondaire

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatExo1::Application.config.secret_token = '82a76e373cc0d66b05315f732f6875d9643c9030505420950dbc01b327e20ac61f23e9a85062ed4df515c9ae2b3ae299c4e0a856ee302848b55b9fc1def078e3'
