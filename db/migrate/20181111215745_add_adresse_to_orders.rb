e331fb010e4f5c1ea3ea7525d4e419c38a7b5e50
mitch olivier <m-olivier@olivier.localb2o.com>
2020-04-20 02:03:18 +0200

    suppression add_adresse_to_orders

--- db/migrate/20181111215745_add_adresse_to_orders.rb
+++
@@ -1,6 +0,0 @@
-class AddAdresseToOrders < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :orders, :payment_address, foreign_key: true
-    add_reference :orders, :delivery_address, foreign_key: true
-  end
-end
