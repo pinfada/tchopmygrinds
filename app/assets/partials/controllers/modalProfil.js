0989bfe90d593acb3810edbf710c3292a89fc5ab
pinfada <cali77@hotmail.fr>
2022-09-25 00:17:23 +0000

    add user profil

---
+++ app/assets/partials/controllers/modalProfil.js
@@ -0,0 +1,18 @@
+marketApp.controller("modalProfil", [
+    '$q', 
+    '$scope',
+    '$uibModalInstance', 
+    '$log', 
+    '$route',
+    'boutique', 
+    'user',
+    'GetCommerceProducts',
+    'GetProductOrders',
+    'myOrderdetails',
+    'UpdateOrderDetails',
+    function ($q, $scope, $uibModalInstance, $log, $route, boutique, user, GetCommerceProducts, GetProductOrders, myOrderdetails, UpdateOrderDetails){
+
+    console.log('ModalProfil')
+
+
+}]);
