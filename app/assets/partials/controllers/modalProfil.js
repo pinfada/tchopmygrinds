844f68a0152dd9aa399f07f8ab8cdad15ccdb908
pinfada <cali77@hotmail.fr>
2022-09-25 23:14:52 +0000

    260922 - Update profil merchant

--- app/assets/partials/controllers/modalProfil.js
+++ app/assets/partials/controllers/modalProfil.js
@@ -13,6 +13,8 @@ marketApp.controller("modalProfil", [
     function ($q, $scope, $uibModalInstance, $log, $route, boutique, user, GetCommerceProducts, GetProductOrders, myOrderdetails, UpdateOrderDetails){
 
     console.log('ModalProfil')
-
+    $scope.foo = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."+ 
+                 "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"+ 
+                 "when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
 }]);
