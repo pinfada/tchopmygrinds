ea5258000ee75e8f0b560daa37b68a8e6000ec70
pinfada <pinfada@github.com>
2025-08-10 12:45:47 +0200

    chore:

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './app/views/**/*.erb',
+    './app/assets/javascripts/**/*.html',
+    './app/assets/javascripts/**/*.erb',
+    './app/assets/partials/**/*.js',
+    './app/assets/partials/**/*.erb'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
+
