a9dcafacbec7b26c216964070f99f9b0fb9c2b13
pinfada <cali77@hotmail.fr>
2026-05-12 01:07:15 +0200

    chore: remove deprecated secret token initializer

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-StatExo1::Application.config.secret_token = 'b3ab19c9f2bc5bc02b9b18157ecc0adb890d847da9cff7a71664f81ac94e4e3ebf5191358ee1a9bde7a51723e1f25f75755bc984cec7051a0127cab015bf573e'
