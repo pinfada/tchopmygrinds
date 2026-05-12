e5a40656ee4ff633937794e74e56c0b50fa33c0b
pinfada <cali77@hotmail.fr>
2026-05-12 21:47:43 +0200

    chore: set Ruby external encoding to UTF-8 in render-build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,10 @@ set -o errexit
 # Forcer l'encodage UTF-8 pour éviter les erreurs sur Render
 export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
+# Fige l'encodage externe Ruby dès le démarrage du process (sinon Sprockets/terser
+# lit certains JS vendor en ASCII-8BIT et casse sur les octets UTF-8 multi-bytes,
+# ex. "\xC2" présent dans rails_admin/popper.js et bootstrap.js).
+export RUBYOPT="-EUTF-8"
 
 bundle install
 # Installer les dépendances frontend, puis générer les assets React/Vite utilisés par pages/react_app.html.erb
